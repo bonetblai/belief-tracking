d0261d6a6648fa733c780e1b1c6f65b086c090f8
bonetblai <bonetblai@f8eec6ff-23cc-1aa9-e85f-827827915515>
2013-01-17 03:06:40 +0000

    Add wumpus example

---
+++ trunk/examples/wumpus/moving_wumpus_belief.h
@@ -0,0 +1,399 @@
+/*
+ *  Copyright (C) 2012 Universidad Simon Bolivar
+ *
+ *  Permission is hereby granted to distribute this software for
+ *  non-commercial research purposes, provided that this copyright
+ *  notice is included with any such distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ *  EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+ *  SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
+ *  ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ *
+ *  Blai Bonet, bonet@ldc.usb.ve
+ *
+ */
+
+#ifndef MOVING_WUMPUS_BELIEF_H
+#define MOVING_WUMPUS_BELIEF_H
+
+#include "grid_belief.h"
+#include "grid_var_beam.h"
+#include "defs.h"
+
+#include <stdlib.h>
+#include <cassert>
+#include <iostream>
+#include <list>
+#include <vector>
+
+class moving_wumpus_belief_t : public grid_belief_t {
+    static int nrows_;
+    static int ncols_;
+    static int npits_;
+    static int nwumpus_;
+    static std::list<moving_wumpus_belief_t*> beliefs_;
+
+  protected:
+    int pos_;
+    int heading_;
+    int narrows_;
+    bool have_gold_;
+    bool dead_;
+
+    grid_var_beam_t gold_;
+    grid_arc_consistency_t pits_;
+    grid_var_beam_t wumpus_;
+
+  public:
+    moving_wumpus_belief_t(int pos = 0, int heading = 0)
+      : grid_belief_t(), pos_(pos), heading_(heading), narrows_(0),
+        have_gold_(false), dead_(false),
+        gold_(1), wumpus_(nwumpus_) {
+        for( int r = 0; r < nrows_; ++r ) {
+            for( int c = 0; c < ncols_; ++c ) {
+                int cell = r * ncols_ + c;
+                pits_.set_domain(cell, new cell_beam_t(r, c, types_[cell]));
+            }
+        }
+        if( nwumpus_ > 1 ) {
+            std::cout << "error: number of wumpus should be at most one!" << std::endl;
+            exit(-1);
+        }
+    }
+    explicit moving_wumpus_belief_t(const moving_wumpus_belief_t &bel)
+      : grid_belief_t(bel), pos_(bel.pos_), heading_(bel.heading_), narrows_(bel.narrows_),
+        have_gold_(bel.have_gold_), dead_(bel.dead_),
+        gold_(bel.gold_), wumpus_(bel.wumpus_) {
+        for( int cell = 0; cell < nrows_ * ncols_; ++cell ) {
+            pits_.set_domain(cell, new cell_beam_t(*bel.pits_.domain(cell)));
+        }
+    }
+    moving_wumpus_belief_t(moving_wumpus_belief_t &&bel)
+      : grid_belief_t(bel), pos_(bel.pos_), heading_(bel.heading_), narrows_(bel.narrows_),
+        have_gold_(bel.have_gold_), dead_(bel.dead_),
+        gold_(std::move(bel.gold_)), wumpus_(std::move(bel.wumpus_)) {
+        for( int cell = 0; cell < nrows_ * ncols_; ++cell ) {
+            pits_.set_domain(cell, bel.pits_.domain(cell));
+            bel.pits_.set_domain(cell, 0);
+        }
+    }
+    virtual ~moving_wumpus_belief_t() { }
+
+    static moving_wumpus_belief_t* allocate() {
+        if( beliefs_.empty() ) {
+            return new moving_wumpus_belief_t;
+        } else {
+            moving_wumpus_belief_t *belief = beliefs_.front();
+            beliefs_.pop_front();
+            assert(belief != 0);
+            belief->clear();
+            return belief;
+        }
+    }
+    static void deallocate(moving_wumpus_belief_t *belief) {
+        if( belief != 0 ) {
+            beliefs_.push_front(belief);
+        }
+    }
+
+    static void initialize(int nrows, int ncols, int npits, int nwumpus) {
+        nrows_ = nrows;
+        ncols_ = ncols;
+        npits_ = npits;
+        nwumpus_ = nwumpus;
+        cell_beam_t::initialize();
+        grid_var_beam_t::initialize(nrows_, ncols_);
+        grid_belief_t::initialize(nrows_, ncols_, grid_belief_t::manhattan_neighbourhood);
+    }
+
+    size_t hash() const { return 0; }
+
+    int position() const { return pos_; }
+    int heading() const { return heading_; }
+    bool have_gold() const { return have_gold_; }
+    bool dead() const { return dead_; }
+    void set_position(int pos) { pos_ = pos; }
+    void set_heading(int heading) { heading_ = heading; }
+
+    bool consistent() const {
+        for( int cell = 0; cell < nrows_ * ncols_; ++cell ) {
+            if( pits_.domain(cell)->empty() ) return false;
+        }
+        return wumpus_.consistent() && (have_gold() || gold_.consistent());
+    }
+
+    bool in_gold_cell() const { return gold_.known() && (*gold_.begin() == pos_); }
+    bool possible_gold_at(int cell) const { return gold_.contains(cell); }
+
+    void clear() {
+        pos_ = 0;
+        heading_ = 0;
+        have_gold_ = false;
+        dead_ = false;
+        gold_.clear();
+        wumpus_.clear();
+        for( int cell = 0; cell < rows_ * cols_; ++cell ) {
+            pits_.domain(cell)->clear();
+        }
+    }
+
+    void set_initial_configuration() {
+        gold_.set_initial_configuration();
+        wumpus_.set_initial_configuration();
+        for( int cell = 0; cell < nrows_ * ncols_; ++cell ) {
+            pits_.domain(cell)->set_initial_configuration(grid_belief_t::manhattan_neighbourhood);
+        }
+    }
+
+    const moving_wumpus_belief_t& operator=(const moving_wumpus_belief_t &bel) {
+        pos_ = bel.pos_;
+        heading_ = bel.heading_;
+        have_gold_ = bel.have_gold_;
+        dead_ = bel.dead_;
+        gold_ = bel.gold_;
+        wumpus_ = bel.wumpus_;
+        for( int cell = 0; cell < nrows_ * ncols_; ++cell ) {
+            assert(pits_.domain(cell) != 0);
+            *pits_.domain(cell) = *bel.pits_.domain(cell);
+        }
+        return *this;
+    }
+
+    bool operator==(const moving_wumpus_belief_t &bel) const {
+        for( int cell = 0; cell < nrows_ * ncols_; ++cell ) {
+            if( *pits_.domain(cell) != *bel.pits_.domain(cell) ) return false;
+        }
+        return (pos_ == bel.pos_) && (heading_ == bel.heading_) &&
+               (have_gold_ == bel.have_gold_) && (dead_ == bel.dead_) &&
+               (gold_ == bel.gold_) && (wumpus_ == bel.wumpus_);
+    }
+    bool operator!=(const moving_wumpus_belief_t &bel) const {
+        return *this == bel ? false : true;
+    }
+
+    void print(std::ostream &os) const {
+        os << "pos=(" << (pos_ % ncols_)
+           << "," << (pos_ / ncols_)
+           << "," << heading_name(heading_) << ")"
+           << ", have-gold=" << (have_gold_ ? "true" : "false")
+           << ", dead=" << (dead_ ? "true" : "false")
+           << std::endl;
+        os << "gold=" << gold_ << std::endl;
+        os << "wumpus=" << wumpus_ << std::endl;
+#if 1
+        for( int r = 0; r < nrows_; ++r ) {
+            for( int c = 0; c < ncols_; ++c ) {
+                int cell = r * ncols_ + c;
+                os << "pbeam(" << c << "," << r << ")=" << *pits_.domain(cell) << std::endl;
+            }
+        }
+#endif
+    }
+
+    int target_cell(int action) const {
+        return ::target_cell(pos_, heading_, action, nrows_, ncols_, false);
+    }
+    int target_heading(int action) const {
+        return ::target_heading(heading_, action);
+    }
+
+    bool applicable(int action) const {
+        return applicable_for_position(action) &&
+               applicable_for_gold(action) &&
+               applicable_for_pits(action) &&
+               applicable_for_wumpus(action);
+    }
+
+    bool possible_obs(int action, int obs) const {
+        assert((obs >= 0) && (obs < 105));
+        if( pos_ == OutsideCave ) {
+            return obs == 0;
+        } else if( obs == 104 ) { // Fell into a pit
+            return !no_pit_at(pos_);
+        } else {
+            return possible_obs_for_gold(action, obs) &&
+                   possible_obs_for_pits(action, obs) &&
+                   possible_obs_for_wumpus(action, obs);
+        }
+    }
+
+    void progress(int action) {
+        assert(applicable(action));
+        progress_position(action);
+        progress_gold(action);
+        progress_pits(action);
+        progress_wumpus(action);
+    }
+
+    bool is_dead_obs(int obs) const {
+        // wumpus at cell (52 = 4 * 13) or fell into pit (104)
+        return (obs == 52) || (obs == 104);
+    }
+
+    void filter(int action, int obs) {
+        grid_var_beam_t before(wumpus_);
+        if( is_dead_obs(obs) ) {
+            dead_ = true;
+        } else {
+            if( pos_ != OutsideCave ) {
+                filter_gold(action, obs);
+                filter_pits(action, obs);
+                filter_wumpus(action, obs);
+            }
+        }
+        assert(consistent());
+    }
+
+    void progress_and_filter(int action, int obs) {
+        progress(action);
+        filter(action, obs);
+    }
+
+    // Knowledge-query methods
+    bool pit_at(int cell) const { return pits_.domain(cell)->obj_at(); }
+    bool wumpus_at(int cell) const { return wumpus_.necessary_obj_at(cell); }
+    bool hazard_at(int cell) const { return pit_at(cell) || wumpus_at(cell); }
+    bool no_pit_at(int cell) const { return pits_.domain(cell)->no_obj_at(); }
+    bool no_wumpus_at(int cell) const { return wumpus_.necessary_no_obj_at(cell); }
+    bool no_hazard_at(int cell) const { return no_pit_at(cell) && no_wumpus_at(cell); }
+
+  private:
+    bool applicable_for_position(int action) const {
+        if( dead_ || (pos_ == OutsideCave) || (action == ActionNoop) ) {
+            return false;
+        } else if( action == ActionShoot ) {
+            return narrows_ > 0;
+        } else if( action == ActionExit ) {
+            return pos_ == 0;
+        } else if( action == ActionMoveForward ) {
+            return target_cell(action) != pos_;
+        } else {
+            return true;
+        }
+    }
+    void progress_position(int action) {
+        if( action == ActionExit ) {
+            pos_ = OutsideCave;
+        } else if( action == ActionMoveForward ) {
+            pos_ = target_cell(action);
+        } else if( (action == ActionTurnLeft) || (action == ActionTurnRight) ) {
+            heading_ = target_heading(action);
+        }
+    }
+
+    bool applicable_for_gold(int action) const {
+        return (action != ActionGrab) || in_gold_cell();
+    }
+    void progress_gold(int action) {
+        if( action == ActionGrab ) {
+            assert(!have_gold());
+            assert(in_gold_cell());
+            gold_.clear();
+            have_gold_ = true;
+        }
+    }
+    bool possible_obs_for_gold(int action, int obs) const {
+        if( obs & Glitter ) {
+            return gold_.possible_obj_at(pos_);
+        } else {
+            return gold_.empty() || gold_.possible_no_obj_at(pos_);
+        }
+    }
+    void filter_gold(int action, int obs) {
+        if( obs & Glitter ) {
+            gold_.filter_obj_at(pos_);
+        } else {
+            gold_.filter_no_obj_at(pos_);
+        }
+    }
+
+    bool applicable_for_pits(int action) const {
+        return true;
+    }
+    void progress_pits(int action) {
+        // nothing to do
+    }
+    bool possible_obs_for_pits(int action, int obs) const {
+        std::pair<int, int> min_max_pits = pits_.domain(pos_)->num_surrounding_objs();    
+        if( obs & Breeze ) {
+            return min_max_pits.second > 0;
+        } else {
+            return min_max_pits.first == 0;
+        }
+    }
+    void filter_pits(int action, int obs) {
+        if( obs & Breeze ) {
+            pit_filter(pos_, 1, true);
+        } else {
+            pit_filter(pos_, 0, false);
+        }
+    }
+
+    bool applicable_for_wumpus(int action) const {
+        return true;
+    }
+    void progress_wumpus(int action) {
+        //if( action < ActionGrab ) {
+        if( action == ActionMoveForward ) {
+            // this is a move action, the wumpuses move non-det in the grid
+            wumpus_.move_non_det();
+        }
+    }
+    bool possible_obs_for_wumpus(int action, int obs) const {
+        assert((obs >= 0) && (obs < 104));
+        int wumpus_obs = obs >> 2;
+        if( wumpus_obs == 0 ) {
+            return wumpus_.possible_no_obj_adjacent_to(pos_, grid_var_beam_t::octile_neighbourhood, 2);
+        } else {
+            --wumpus_obs;
+            int drow = (wumpus_obs / 5) - 2, dcol = (wumpus_obs % 5) - 2;
+            int row = pos_ / ncols_, col = pos_ % ncols_;
+            int wrow = row + drow, wcol = col + dcol;
+            if( (wrow >= 0) && (wrow < nrows_) && (wcol >= 0) && (wcol < ncols_) ) {
+                int wpos = wrow * ncols_ + wcol;
+                return wumpus_.possible_obj_at(wpos);
+            } else {
+                return false;
+            }
+        }
+    }
+    void filter_wumpus(int action, int obs) {
+        assert((obs >= 0) && (obs < 104));
+        int wumpus_obs = obs >> 2;
+        if( wumpus_obs == 0 ) {
+            return wumpus_.filter_no_obj_adjacent_to(pos_, grid_var_beam_t::octile_neighbourhood, 2);
+        } else {
+            --wumpus_obs;
+            int drow = (wumpus_obs / 5) - 2, dcol = (wumpus_obs % 5) - 2;
+            int row = pos_ / ncols_, col = pos_ % ncols_;
+            int wrow = row + drow, wcol = col + dcol;
+            assert((wrow >= 0) && (wrow < nrows_) && (wcol >= 0) && (wcol < ncols_));
+            int wpos = wrow * ncols_ + wcol;
+            wumpus_.filter_obj_at(wpos);
+        }
+    }
+
+    void pit_filter(int cell, int nobjs, bool at_least) {
+        std::vector<int> revised_cells;
+        pits_.domain(cell)->filter(nobjs, at_least);
+        pits_.add_to_worklist(cell);
+        pits_.ac3(revised_cells);
+    }
+};
+
+int moving_wumpus_belief_t::nrows_ = 0;
+int moving_wumpus_belief_t::ncols_ = 0;
+int moving_wumpus_belief_t::npits_ = 0;
+int moving_wumpus_belief_t::nwumpus_ = 0;
+std::list<moving_wumpus_belief_t*> moving_wumpus_belief_t::beliefs_;
+
+inline std::ostream& operator<<(std::ostream &os, const moving_wumpus_belief_t &bel) {
+    bel.print(os);
+    return os;
+}
+
+#endif
+
---
+++ trunk/examples/wumpus/wumpus_api.h
@@ -0,0 +1,179 @@
+/*
+ *  Copyright (C) 2012 Universidad Simon Bolivar
+ *
+ *  Permission is hereby granted to distribute this software for
+ *  non-commercial research purposes, provided that this copyright
+ *  notice is included with any such distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ *  EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+ *  SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
+ *  ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ *
+ *  Blai Bonet, bonet@ldc.usb.ve
+ *
+ */
+
+#ifndef WUMPUS_API_H
+#define WUMPUS_API_H
+
+#include "problem.h"
+#include "base_policy.h"
+#include "exact_inference.h"
+
+#include <iostream>
+#include <vector>
+
+#include <dispatcher.h>
+
+struct abstract_api_t {
+    int nrows_;
+    int ncols_;
+    int npits_;
+    int nwumpus_;
+    int narrows_;
+    bool nesw_movements_;
+    int seed_;
+
+    std::string policy_name_;
+    Online::Evaluation::parameters_t eval_pars_;
+
+  public:
+    abstract_api_t(int nrows, int ncols, int npits, int nwumpus, int narrows, bool nesw_movements)
+      : nrows_(nrows), ncols_(ncols), npits_(npits), nwumpus_(nwumpus), narrows_(narrows),
+        nesw_movements_(nesw_movements), seed_(0) {
+        Random::set_seed(seed_);
+    }
+    virtual ~abstract_api_t() { }
+
+    void set_seed(int seed) {
+        seed_ = seed;
+        Random::set_seed(seed_);
+        std::cout << "seed=" << seed_ << std::endl;
+    }
+
+    void set_policy_parameters(int width, int depth, float par1, float par2) {
+        eval_pars_.width_ = width;
+        eval_pars_.depth_ = depth;
+        eval_pars_.par1_ = par1;
+        eval_pars_.par2_ = par2;
+    }
+
+    virtual void select_policy(const std::string &base_name, const std::string &policy_type) = 0;
+    virtual void prepare_new_trial(int heading) = 0;
+    virtual int select_action() const = 0;
+    virtual void update(int obs) = 0;
+    virtual void apply_action_and_update(int action, int obs) = 0;
+    virtual void print(std::ostream &os) const = 0;
+
+    // exact inference
+    virtual bool is_world_explored() const = 0;
+};
+
+template<typename T> struct template_wumpus_api_t : public abstract_api_t {
+    template_problem_t<T> *problem_;
+    T *state_;
+    char *sensed_info_;
+
+    std::vector<std::pair<const Online::Policy::policy_t<T>*, std::string> > bases_;
+    std::vector<std::pair<const Heuristic::heuristic_t<T>*, std::string> > heuristics_;
+
+    Heuristic::heuristic_t<T> *heuristic_;
+    const Online::Policy::policy_t<T> *policy_;
+
+  public:
+    template_wumpus_api_t(int nrows, int ncols, int npits, int nwumpus, int narrows, bool nesw_movements)
+      : abstract_api_t(nrows, ncols, npits, nwumpus, narrows, nesw_movements),
+        problem_(0), state_(0), heuristic_(0), policy_(0) {
+        sensed_info_ = new char[nrows_ * ncols_];
+
+        T::initialize(nrows_, ncols_);
+        wumpus_belief_t::initialize(nrows_, ncols_, npits_, nwumpus_);
+        moving_wumpus_belief_t::initialize(nrows_, ncols_, npits_, nwumpus_);
+
+        problem_ = new template_problem_t<T>(nrows_, ncols_, npits_, nwumpus_, narrows_, GOAL_IS_HAVE_GOLD, 1e5);
+
+        // set heuristic
+        Heuristic::heuristic_t<T> *h = new shortest_distance_to_unvisited_cell_t<T>(*problem_, nesw_movements_);
+        heuristics_.push_back(std::make_pair(h, "shortest_distance_to_unvisited_cell_heuristic"));
+
+
+        // set base policies
+        Online::Policy::greedy_t<T> greedy(*problem_, *heuristic_);
+        bases_.push_back(std::make_pair(greedy.clone(), "greedy_wrt_sduv-heuristic"));
+        Online::Policy::random_greedy_t<T> random_greedy(*problem_, *heuristic_);
+        bases_.push_back(std::make_pair(random_greedy.clone(), "random-greedy_wrt_sduv-heuristic"));
+        Online::Policy::optimistic_greedy_t<T> optimistic_greedy(*problem_, *heuristic_);
+        bases_.push_back(std::make_pair(optimistic_greedy.clone(), "optimistic-greedy_wrt_sduv-heuristic"));
+        Online::Policy::random_t<T> random(*problem_);
+        bases_.push_back(std::make_pair(random.clone(), "random"));
+        //wumpus_base_policy_t<T> wumpus(*problem_, nrows_, ncols_, nesw_movements_);
+        //bases_.push_back(std::make_pair(wumpus.clone(), "wumpus_base"));
+    }
+    virtual ~template_wumpus_api_t() {
+        delete[] sensed_info_;
+        delete heuristic_;
+        delete problem_;
+    }
+
+    virtual void select_policy(const std::string &base_name, const std::string &policy_type) {
+        std::pair<const Online::Policy::policy_t<T>*, std::string> p =
+          Online::Evaluation::select_policy(*problem_, base_name, policy_type, bases_, heuristics_, eval_pars_);
+        policy_ = p.first;
+        policy_name_ = p.second;
+        //std::cout << "template_wumpus_api_t::policy=" << policy_name_ << std::endl;
+    }
+
+    virtual void prepare_new_trial(int heading) {
+        memset(sensed_info_, 255, nrows_ * ncols_);
+        if( state_ ) delete state_;
+        state_ = new T(0, heading);
+        //state_->set_narrows(narrows_);
+        state_->set_initial_configuration();
+    }
+
+    virtual int select_action() const {
+        Problem::action_t action = (*policy_)(*state_);
+        assert(action != Problem::noop);
+        assert(state_->applicable(action));
+        //std::cout << "pos=(" << (state_->pos() % ncols_) << ","
+        //          << (state_->pos() / ncols_) << ")"
+        //          << ", heading=" << heading_name(state_->heading())
+        //          << std::endl;
+        return action;
+    }
+
+    virtual void update(int obs) {
+        state_->update(-1, obs);
+        update_sensed_info(state_->position(), obs);
+        //std::cout << *state_;
+    }
+    virtual void apply_action_and_update(int action, int obs) {
+        state_->apply_action_and_update(action, obs);
+        update_sensed_info(state_->position(), obs);
+        //std::cout << *state_;
+    }
+
+    virtual void print(std::ostream &os) const {
+        //os << *state_;
+        //os << "heuristic=" << heuristic_->value(*state_) << std::endl;
+    }
+
+    // exact inference
+    void update_sensed_info(int pos, int obs) {
+        sensed_info_[pos] = obs;
+    }
+
+    virtual bool is_world_explored() const {
+        exact_inference_t inference(nrows_, ncols_);
+        return inference.is_world_explored(sensed_info_);
+    }
+};
+
+typedef template_wumpus_api_t<state_t> wumpus_api_t;
+typedef template_wumpus_api_t<moving_state_t> moving_wumpus_api_t;
+
+#endif
+
