a647b668dc1531680fffffe8ff4c775bb9579617
bonetblai <bonetblai@gmail.com>
2013-02-16 23:47:24 +0000

    Add main for mines

---
+++ examples/mines/main.cc
@@ -0,0 +1,248 @@
+/*
+ *  Copyright (C) 2013 Universidad Simon Bolivar
+ *
+ *  Permission is hereby granted to distribute this software for
+ *  non-commercial research purposes, provided that this copyright
+ *  notice is included with any such distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ *  EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+ *  SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
+ *  ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ *
+ *  Blai Bonet, bonet@ldc.usb.ve
+ *
+ */
+
+#include <cassert>
+#include <string>
+#include <iostream>
+#include <string.h>
+#include <set>
+#include <vector>
+#include <stdlib.h>
+
+extern "C" {
+#include "c_api.h"
+};
+
+using namespace std;
+
+struct cell_t {
+    bool mine_;
+    int nmines_;
+    cell_t() : mine_(false), nmines_(0) { }
+};
+
+struct field_t {
+    int nrows_;
+    int ncols_;
+    int ncells_;
+    int nmines_;
+    int num_remaining_mines_;
+
+    vector<cell_t> cells_;
+
+    field_t(int nrows, int ncols, int nmines)
+      : nrows_(nrows), ncols_(ncols), ncells_(nrows_ * ncols_), nmines_(nmines) {
+    }
+
+    int num_remaining_mines() const { return num_remaining_mines_; }
+
+    void sample_field(int initial_cell) {
+        vector<int> available_cells(ncells_, 0);
+        for( int i = 0; i < ncells_; ++i )
+            available_cells[i] = i;
+
+        // do not place a mine at the initial cell or surrounding cells
+        set<int> forbidden;
+        int r = initial_cell / ncols_, c = initial_cell % ncols_;
+        for( int dr = -1; dr < 2; ++dr ) {
+            int nr = r + dr;
+            if( (nr < 0) || (nr >= nrows_) ) continue;
+            for( int dc = -1; dc < 2; ++dc ) {
+                int nc = c + dc;
+                int cell = nr * ncols_ + nc;
+                if( (nc < 0) || (nc >= ncols_) ) continue;
+                forbidden.insert(-cell);
+            }
+        }
+        for( set<int>::iterator it = forbidden.begin(); it != forbidden.end(); ++it ) {
+            int pos = -(*it);
+            available_cells[pos] = available_cells.back();
+            available_cells.pop_back();
+        }
+
+        // place random mines in other cells
+        cells_ = vector<cell_t>(ncells_);
+        for( int num_placed_mines = 0; num_placed_mines < nmines_; ++num_placed_mines ) {
+            int pos = lrand48() % available_cells.size();
+            int cell = available_cells[pos];
+            available_cells[pos] = available_cells.back();
+            available_cells.pop_back();
+            assert(!cells_[cell].mine_);
+            cells_[cell].mine_ = true;
+            int r = cell / ncols_, c = cell % ncols_;
+            for( int dr = -1; dr < 2; ++dr ) {
+                int nr = r + dr;
+                if( (nr < 0) || (nr >= nrows_) ) continue;
+                for( int dc = -1; dc < 2; ++dc ) {
+                    int nc = c + dc;
+                    int ncell = nr * ncols_ + nc;
+                    if( (nc < 0) || (nc >= ncols_) ) continue;
+                    if( cell == ncell ) continue;
+                    ++cells_[ncell].nmines_;
+                }
+            }
+        }
+        num_remaining_mines_ = nmines_;
+        //print(cout);
+    }
+
+    int flag_cell(int cell, bool verbose) {
+        int r = cell / ncols_, c = cell % ncols_;
+        if( verbose ) cout << "flag_cell(" << c << "," << r << "): mine=" << (cells_[cell].mine_ ? 1 : 0) << endl;
+        num_remaining_mines_ -= cells_[cell].mine_ ? 1 : 0;
+        return -1;
+    }
+
+    int open_cell(int cell, bool verbose) {
+        int r = cell / ncols_, c = cell % ncols_;
+        if( verbose ) cout << "open_cell(" << c << "," << r << "): mine=" << (cells_[cell].mine_ ? 1 : 0) << ", #mines=" << cells_[cell].nmines_ << endl;
+        return cells_[cell].mine_ ? 9 : cells_[cell].nmines_;
+    }
+
+    int apply_action(int action, bool verbose) {
+        int cell = agent_get_cell(action);
+        if( agent_is_flag_action(action) ) {
+            return flag_cell(cell, verbose);
+        } else {
+            return open_cell(cell, verbose);
+        }
+    }
+
+    void print(ostream &os) const {
+        for( int c = 0; c < ncols_; ++c ) {
+            for( int r = 0; r < nrows_; ++r ) {
+                int cell = r * ncols_ + c;
+                if( cells_[cell].mine_ )
+                    os << " *";
+                else
+                    os << " " << cells_[cell].nmines_;
+            }
+            os << endl;
+        }
+    }
+};
+
+void usage(ostream &os) {
+    os << "usage: mines"
+       << " [{-t|--ntrials} <int>]"
+       << " [{-r|--nrows} <int>]"
+       << " [{-c|--ncols} <int>]"
+       << " [{-m|--nmines} <int>]"
+       << " [{-s|--seed} <int>]"
+       << " [{-v|--verbose}]"
+       << " [{-p|--policy} <policy-description>]"
+       << " [{-w|--width} <int>]"
+       << " [{-d|--depth} <int>]"
+       << " [{-?|--help}]"
+       << endl;
+}
+
+int main(int argc, const char **argv) {
+    int ntrials = 1;
+    int nrows = 10;
+    int ncols = 10;
+    int nmines = 10;
+    int seed = 0;
+    bool verbose = false;
+
+    string policy = "base-policy:direct";
+    int width = 100;
+    int depth = 10;
+
+    --argc;
+    ++argv;
+    while( (argc > 0) && (**argv == '-') ) {
+        if( !strcmp(argv[0], "-t") || !strcmp(argv[0], "--ntrials") ) {
+            ntrials = atoi(argv[1]);
+            argc -= 2;
+            argv += 2;
+        } else if( !strcmp(argv[0], "-r") || !strcmp(argv[0], "--nrows") ) {
+            nrows = atoi(argv[1]);
+            argc -= 2;
+            argv += 2;
+        } else if( !strcmp(argv[0], "-c") || !strcmp(argv[0], "--ncols") ) {
+            ncols = atoi(argv[1]);
+            argc -= 2;
+            argv += 2;
+        } else if( !strcmp(argv[0], "-m") || !strcmp(argv[0], "--nmines") ) {
+            nmines = atoi(argv[1]);
+            argc -= 2;
+            argv += 2;
+        } else if( !strcmp(argv[0], "-s") || !strcmp(argv[0], "--seed") ) {
+            seed = atoi(argv[1]);
+            argc -= 2;
+            argv += 2;
+        } else if( !strcmp(argv[0], "-v") || !strcmp(argv[0], "--verbose") ) {
+            verbose = true;
+            --argc;
+            ++argv;
+        } else if( !strcmp(argv[0], "-p") || !strcmp(argv[0], "--policy") ) {
+            policy = argv[1];
+            argc -= 2;
+            argv += 2;
+        } else if( !strcmp(argv[0], "-w") || !strcmp(argv[0], "--width") ) {
+            width = atoi(argv[1]);
+            argc -= 2;
+            argv += 2;
+        } else if( !strcmp(argv[0], "-d") || !strcmp(argv[0], "--depth") ) {
+            depth = atoi(argv[1]);
+            argc -= 2;
+            argv += 2;
+        } else if( !strcmp(argv[0], "-?") || !strcmp(argv[0], "--help") ) {
+            usage(cout);
+            exit(-1);
+        } else {
+            cout << "error: unexpected argument: " << argv[0] << endl;
+            --argc;
+            ++argv;
+        }
+    }
+
+    // set seed
+    unsigned short seeds[3];
+    seeds[0] = seeds[1] = seeds[2] = seed;
+    seed48(seeds);
+
+    // run for the specified number of trials
+    for( int trial = 0; trial < ntrials; ++trial ) {
+        field_t field(nrows, ncols, nmines);
+        agent_initialize(nrows, ncols, nmines);
+        bool win = true;
+        for( int plays = 0; plays < nrows * ncols; ++plays ) {
+            int action = agent_get_action();
+            if( plays == 0 ) {
+                assert(!agent_is_flag_action(action));
+                int cell = agent_get_cell(action);
+                field.sample_field(cell);
+                assert(!field.cells_[cell].mine_);
+            }
+            int obs = field.apply_action(action, verbose);
+            if( obs == 9 ) {
+                win = false;
+                break;
+            }
+            agent_update_state(agent_is_flag_action(action), agent_get_cell(action), obs);
+        }
+        if( win )
+            agent_declare_win();
+        else
+            agent_declare_lose();
+    }
+    return 0;
+}
+
