92973634aa07520a04d9c1275a5ffa86540d2401
bonetblai@gmail.com <bonetblai@gmail.com@f8eec6ff-23cc-1aa9-e85f-827827915515>
2014-12-15 03:25:17 +0000

    working

--- trunk/examples/mines/c_api.h
+++ trunk/examples/mines/c_api.h
@@ -1,6 +1,6 @@
 void agent_initialize(int rows, int cols, int nmines);
 void agent_finalize();
-int agent_get_action();
+int agent_get_action(int *is_guess);
 int agent_is_flag_action(int action);
 int agent_get_cell(int action);
 int agent_get_nguesses();
