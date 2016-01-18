5feb740aefd657d56179bc894e6d97c5940058c5
Blai Bonet <blaibonet@imac27.local>
2016-01-17 22:36:25 -0430

    update experiment scripts and todo list

--- examples/probabilistic/slam3/experiment.sh
+++ examples/probabilistic/slam3/experiment.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ $# -ne 9 ]; then
-  echo "usage: experiment.sh [ore|aisle] [mm|opt] <ncols> <nrows> <nparticles> <ntrials> <policy-parameter> <po> <level>"
+if [ $# -ne 8 ]; then
+  echo "usage: experiment.sh [ore-slam-peaked|ore-slam-non-peaked|aisle-slam] [mm|opt] <ncols> <nrows> <nparticles> <ntrials> <policy-parameter> <po>"
   exit 1
 fi
 
-slam=$1
+slam_type=$1
 shift 1
 type=$1
 nc=$2
@@ -14,12 +14,12 @@ np=$4
 nt=$5
 p=$6
 po=$7
-level=$8
 
-./slam --${slam}-slam --seed $RANDOM -c $nc -r $nr -p 1 $p --pa .9 --po $po --kappa .1 -t $nt \
-    --tracker="${type}-rbpf(nparticles=$np,force-resampling=true,sus=false,num-sampling-attempts=2,use-ac3=true,iterated-level=${level})"
-#    --tracker="${type}-rbpf(nparticles=$np,force-resampling=true,sus=false,num-sampling-attempts=2,inference=jt(updates=HUGIN))" \
-#   --tracker="${type}-rbpf(nparticles=$np,force-resampling=true,sus=false,num-sampling-attempts=2,inference=bp(updates=SEQRND,logdomain=false,tol=1e-5,maxtime=1))"
-#    --tracker="${type}-rbpf(nparticles=$np,force-resampling=true,sus=false,num-sampling-attempts=2,inference=hak(doubleloop=true,clusters=MIN,init=UNIFORM,tol=1e-3,maxiter=100,maxtime=1))"
-
+./slam --${slam_type} --seed $RANDOM -c $nc -r $nr -p 1 $p --pa .9 --po $po --kappa .1 -t $nt \
+    --tracker="${type}-rbpf(nparticles=$np,force-resampling=true,sus=false,num-sampling-attempts=2,inference=jt(updates=HUGIN))" \
+    --tracker="${type}-rbpf(nparticles=$np,force-resampling=true,sus=false,num-sampling-attempts=2,inference=bp(updates=SEQRND,logdomain=false,tol=1e-5,maxtime=.5))" \
+    --tracker="${type}-rbpf(nparticles=$np,force-resampling=true,sus=false,num-sampling-attempts=2,inference=iterated-ac3(level=0,inverse-check=true))" \
+    --tracker="${type}-rbpf(nparticles=$np,force-resampling=true,sus=false,num-sampling-attempts=2,inference=iterated-ac3(level=1,inverse-check=true))" \
+    --tracker="${type}-rbpf(nparticles=$np,force-resampling=true,sus=false,num-sampling-attempts=2,inference=iterated-ac3(level=2,inverse-check=true))"
+#    --tracker="${type}-rbpf(nparticles=$np,force-resampling=true,sus=false,num-sampling-attempts=2,inference=hak(doubleloop=true,clusters=MIN,init=UNIFORM,tol=1e-3,maxiter=100,maxtime=1))" \
 
