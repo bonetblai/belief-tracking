57af0885746a860f0df47997955f45cc70565a92
bonetblai <blaibonet@mba.local>
2016-01-19 09:11:53 -0430

    fix single.sh in sge-scripts

--- examples/probabilistic/slam3/sge-scripts/single.sh
+++ examples/probabilistic/slam3/sge-scripts/single.sh
@@ -4,6 +4,7 @@ parameters=$1
 output_dir=$2
 nr=`wc -l $parameters | awk '{ print $1; }'`
 
+mkdir -p output
 sed "s/XXX/$nr/" < slam-template.sub > tmp.sub
 qsub tmp.sub ${parameters}.txt sge-results/$output_dir
 rm tmp.sub
