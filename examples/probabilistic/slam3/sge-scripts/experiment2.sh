f8e64cf5e357337332d88b607e86d0a687fe9504
bonetblai <blaibonet@mba.local>
2016-01-19 09:26:12 -0430

    fix sge-scripts

--- examples/probabilistic/slam3/sge-scripts/experiment2.sh
+++ examples/probabilistic/slam3/sge-scripts/experiment2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 7 ]; then
-  echo "usage: experiment.sh [ore-slam-peaked|ore-slam-non-peaked|aisle-slam] [mm|opt] <ncols> <nrows> <ntrials> <policy-parameter> <po>"
+if [ $# -ne 8 ]; then
+  echo "usage: experiment.sh [ore-slam-peaked|ore-slam-non-peaked|aisle-slam] [mm|opt] <ncols> <nrows> <ntrials> <policy-parameter> <po> <kappa>"
   exit 1
 fi
 
@@ -13,11 +13,16 @@ nr=$3
 nt=$4
 p=$5
 po=$6
+kappa=$7
 
-./slam --${slam_type} --seed $RANDOM -c $nc -r $nr -p 1 $p --pa .9 --po $po --kappa .1 -t $nt \
-    --tracker="${type}-rbpf(nparticles=64,force-resampling=true,sus=false,num-sampling-attempts=2,inference=jt(updates=HUGIN))" \
-    --tracker="${type}-rbpf(nparticles=128,force-resampling=true,sus=false,num-sampling-attempts=2,inference=jt(updates=HUGIN))" \
-    --tracker="${type}-rbpf(nparticles=256,force-resampling=true,sus=false,num-sampling-attempts=2,inference=jt(updates=HUGIN))" \
-    --tracker="${type}-rbpf(nparticles=512,force-resampling=true,sus=false,num-sampling-attempts=2,inference=jt(updates=HUGIN))" \
-    --tracker="${type}-rbpf(nparticles=64,force-resampling=true,sus=false,num-sampling-attempts=2,inference=iterated-ac3(level=0,inverse-check=true))"
+root=$HOME/software/github/belief-tracking/examples/probabilistic/slam3
+$root/slam --${slam_type} --seed $RANDOM -c $nc -r $nr -p 1 $p --pa .9 --po $po --kappa $kappa -t $nt \
+    --tracker="${type}-rbpf(nparticles=64,force-resampling=true,sus=false,inference=jt(updates=HUGIN))" \
+    --tracker="${type}-rbpf(nparticles=128,force-resampling=true,sus=false,inference=jt(updates=HUGIN))" \
+    --tracker="${type}-rbpf(nparticles=256,force-resampling=true,sus=false,inference=jt(updates=HUGIN))" \
+    --tracker="${type}-rbpf(nparticles=512,force-resampling=true,sus=false,inference=jt(updates=HUGIN))" \
+    --tracker="${type}-rbpf(nparticles=64,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))" \
+    --tracker="${type}-rbpf(nparticles=128,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))" \
+    --tracker="${type}-rbpf(nparticles=256,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))" \
+    --tracker="${type}-rbpf(nparticles=512,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))" \
 
