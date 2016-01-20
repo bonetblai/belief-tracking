11c11e6b78cfe03987488760a1b460f985989807
Blai BONET <bbonet@deploy>
2016-01-20 19:38:01 +0100

    update scripts

--- examples/probabilistic/slam3/sge-scripts/generate_stats_csv.sh
+++ examples/probabilistic/slam3/sge-scripts/generate_stats_csv.sh
@@ -2,19 +2,16 @@
 
 string=$1
 raw_file=$2
-output=$3
+stype=$3
+extra=$4
 
-extracted=tmp.$output.txt
+extracted=tmp.txt
 grep $string $raw_file | sed "s/=/ /g" | sed "s/,/ /g" > $extracted
 nf=`head -1 $extracted | awk '{ print NF; }'`
 
-echo "s1,file,steps,unknowns,errors,time" > $output.csv
-if [ "$nf" == "51" ]; then # this is AC3
-  awk '{ printf "%s,%s,%s,%s,%s,%s\n", s1, s2, $21, $(NF-4), $(NF-2), $NF; }' s1=$output s2=$raw_file < $extracted >> $output.csv
-else
-  awk '{ printf "%s,%s,%s,%s,%s,%s\n", s1, s2, $24, $(NF-4), $(NF-2), $NF; }' s1=$output s2=$raw_file < $extracted >> $output.csv
-fi
-echo `wc -l $output.csv | awk '{ print $1; }'` "record(s) generated in '$output.csv'"
+#echo "inference,file,type,sir,dim,np,steps,unknowns,errors,time"
+awk '{ for(i=1;i<=NF;++i) { if( $i == "#steps" ) ns=$(i+1); if( $i == "#unknowns" ) nu=$(i+1); if( $i == "#errors" ) ne=$(i+1); if( $i == "elapsed-time" ) et=$(i+1); } } printf "%s,\"%s\",%s,%s,%s,%s,%s\n", s1, s2, s3, ns, nu, ne, et; }' s1=$stype s2=$raw_file s3=$extra < $extracted
 
 rm $extracted
+
 
