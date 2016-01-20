f0c0d4bc32adc48fb1157d4da2bf0b60ec477c2c
Blai Bonet <blaibonet@imac27.local>
2016-01-20 13:58:50 -0430

    change to experiment scripts

--- examples/probabilistic/slam3/sge-scripts/experiment2.sh
+++ examples/probabilistic/slam3/sge-scripts/experiment2.sh
@@ -17,12 +17,22 @@ kappa=$7
 
 root=$HOME/software/github/belief-tracking/examples/probabilistic/slam3
 $root/slam --${slam_type} --seed $RANDOM -c $nc -r $nr -p 1 $p --pa .9 --po $po --kappa $kappa -t $nt \
+    --tracker="${type}-rbpf(nparticles=32,force-resampling=true,sus=false,inference=jt(updates=HUGIN))" \
     --tracker="${type}-rbpf(nparticles=64,force-resampling=true,sus=false,inference=jt(updates=HUGIN))" \
     --tracker="${type}-rbpf(nparticles=128,force-resampling=true,sus=false,inference=jt(updates=HUGIN))" \
     --tracker="${type}-rbpf(nparticles=256,force-resampling=true,sus=false,inference=jt(updates=HUGIN))" \
     --tracker="${type}-rbpf(nparticles=512,force-resampling=true,sus=false,inference=jt(updates=HUGIN))" \
+    --tracker="${type}-rbpf(nparticles=1024,force-resampling=true,sus=false,inference=jt(updates=HUGIN))" \
+    --tracker="${type}-rbpf(nparticles=32,force-resampling=true,sus=false,inference=bp(updates=SEQRND,logdomain=false,tol=1e-5,maxtime=.5))" \
+    --tracker="${type}-rbpf(nparticles=64,force-resampling=true,sus=false,inference=bp(updates=SEQRND,logdomain=false,tol=1e-5,maxtime=.5))" \
+    --tracker="${type}-rbpf(nparticles=128,force-resampling=true,sus=false,inference=bp(updates=SEQRND,logdomain=false,tol=1e-5,maxtime=.5))" \
+    --tracker="${type}-rbpf(nparticles=256,force-resampling=true,sus=false,inference=bp(updates=SEQRND,logdomain=false,tol=1e-5,maxtime=.5))" \
+    --tracker="${type}-rbpf(nparticles=512,force-resampling=true,sus=false,inference=bp(updates=SEQRND,logdomain=false,tol=1e-5,maxtime=.5))" \
+    --tracker="${type}-rbpf(nparticles=1024,force-resampling=true,sus=false,inference=bp(updates=SEQRND,logdomain=false,tol=1e-5,maxtime=.5))" \
+    --tracker="${type}-rbpf(nparticles=32,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))" \
     --tracker="${type}-rbpf(nparticles=64,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))" \
     --tracker="${type}-rbpf(nparticles=128,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))" \
     --tracker="${type}-rbpf(nparticles=256,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))" \
-    --tracker="${type}-rbpf(nparticles=512,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))"
+    --tracker="${type}-rbpf(nparticles=512,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))" \
+    --tracker="${type}-rbpf(nparticles=1024,force-resampling=true,sus=false,inference=iterated-ac3(level=0,inverse-check=true))"
 
