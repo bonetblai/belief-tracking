b00fd8b1551e750510baf57a81ffcfee1607de11
bonetblai <blaibonet@mba.local>
2016-01-26 16:13:40 -0600

    fix parse script for slam3

--- examples/probabilistic/slam3/sge-scripts/parse_stats.sh
+++ examples/probabilistic/slam3/sge-scripts/parse_stats.sh
@@ -5,7 +5,7 @@ for slam_type in ore-slam-peaked ore-slam-non-peaked; do
   for sir in opt mm; do
     for dim in 4x4 6x6 8x8 10x10 12x12 14x14; do
       for np in 32 64 128 256; do
-        for dir in results/*; do
+        for dir in results/parameters-?n*.txt; do
           ./generate_stats_csv.sh HUGIN $dir/raw_data.type=${slam_type}.sir=${sir}.dim=${dim}.np=${np}.kappa=.1.txt $slam_type "jt,${sir},${dim},${np}"
           ./generate_stats_csv.sh SEQRND $dir/raw_data.type=${slam_type}.sir=${sir}.dim=${dim}.np=${np}.kappa=.1.txt $slam_type "bp,${sir},${dim},${np}"
           ./generate_stats_csv.sh "level=0" $dir/raw_data.type=${slam_type}.sir=${sir}.dim=${dim}.np=${np}.kappa=.1.txt $slam_type "ac(level=0),${sir},${dim},${np}"
