057fefbed1b719a472a979531a54c0c267cb5ae2
bonetblai <blaibonet@mba.local>
2016-01-26 20:10:36 -0500

    add scripts for experiments with executions

--- examples/probabilistic/slam3/sge-scripts/single-ex-ac0.sh
+++ examples/probabilistic/slam3/sge-scripts/single-ex-ac0.sh
@@ -5,7 +5,7 @@ output_dir=$2
 nr=`wc -l $parameters | awk '{ print $1; }'`
 
 mkdir -p output
-sed "s/XXX/$nr/" < slam-template-ex.sub > tmp.sub
+sed "s/XXX/$nr/" < slam-template-ex-ac0.sub > tmp.sub
 qsub tmp.sub $parameters $output_dir
 rm tmp.sub
 
