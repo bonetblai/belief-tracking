7355c7f4c0735bf56fe7baedd01ad4e8aadcaf3f
Blai Bonet <blaibonet@imac27.local>
2016-01-28 00:49:17 -0430

    fix parse scripts

--- examples/probabilistic/slam3/sge-scripts/parse_stats.sh
+++ examples/probabilistic/slam3/sge-scripts/parse_stats.sh
@@ -7,7 +7,8 @@ for slam_type in ore-slam-peaked ore-slam-non-peaked; do
       for np in 32 64 128 256; do
         for dir in results/parameters-?n*.txt; do
           ./generate_stats_csv.sh HUGIN $dir/raw_data.type=${slam_type}.sir=${sir}.dim=${dim}.np=${np}.kappa=.1.txt $slam_type "jt,${sir},${dim},${np}"
-          ./generate_stats_csv.sh SEQRND $dir/raw_data.type=${slam_type}.sir=${sir}.dim=${dim}.np=${np}.kappa=.1.txt $slam_type "bp,${sir},${dim},${np}"
+          ./generate_stats_csv.sh "SEQRND,logdomain=false,tol=1e-5,maxtime=.1" $dir/raw_data.type=${slam_type}.sir=${sir}.dim=${dim}.np=${np}.kappa=.1.txt $slam_type "bp(maxtime=.1),${sir},${dim},${np}"
+          ./generate_stats_csv.sh "SEQRND,logdomain=false,tol=1e-5,maxtime=.5" $dir/raw_data.type=${slam_type}.sir=${sir}.dim=${dim}.np=${np}.kappa=.1.txt $slam_type "bp(maxtime=.5),${sir},${dim},${np}"
           ./generate_stats_csv.sh "level=0" $dir/raw_data.type=${slam_type}.sir=${sir}.dim=${dim}.np=${np}.kappa=.1.txt $slam_type "ac(level=0),${sir},${dim},${np}"
           ./generate_stats_csv.sh "level=1" $dir/raw_data.type=${slam_type}.sir=${sir}.dim=${dim}.np=${np}.kappa=.1.txt $slam_type "ac(level=1),${sir},${dim},${np}"
           ./generate_stats_csv.sh "level=2" $dir/raw_data.type=${slam_type}.sir=${sir}.dim=${dim}.np=${np}.kappa=.1.txt $slam_type "ac(level=2),${sir},${dim},${np}"
