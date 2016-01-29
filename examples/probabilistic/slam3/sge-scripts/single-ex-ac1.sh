c6420e6cddc6f4acd02e239be5e5075d9387b8fc
Blai BONET <bbonet@node12.upf.edu>
2016-01-29 19:22:51 +0100

    add kappa to scripts and fix path for executions

--- examples/probabilistic/slam3/sge-scripts/single-ex-ac1.sh
+++ examples/probabilistic/slam3/sge-scripts/single-ex-ac1.sh
@@ -2,10 +2,11 @@
 
 parameters=$1
 output_dir=$2
+kappa=$3
 nr=`wc -l $parameters | awk '{ print $1; }'`
 
 mkdir -p output
 sed "s/XXX/$nr/" < slam-template-ex-ac1.sub > tmp.sub
-qsub tmp.sub $parameters $output_dir
+qsub tmp.sub $parameters $output_dir $kappa
 rm tmp.sub
 
